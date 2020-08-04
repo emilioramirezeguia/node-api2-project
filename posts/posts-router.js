aa59c98375fca37a3eba39684d792b80c28dbc48
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-08-04 12:42:55 -0700

    Set up Express router for '/api/posts' endpoints

---
+++ posts/posts-router.js
@@ -0,0 +1,63 @@
+const router = require("express").Router();
+
+// Returns an array of all the post objects contained in the database.
+router.get("/", (req, res) => {
+  res.status(200).json({ router: "posts", url: "/api/posts", method: "GET" });
+});
+
+// Creates a post using the information sent inside the request body.
+router.post("/", (req, res) => {
+  res.status(200).json({ router: "posts", url: "/api/posts", method: "POST" });
+});
+
+// 	Returns the post object with the specified id.
+router.get("/:id", (req, res) => {
+  res.status(200).json({
+    router: "posts",
+    url: "/api/posts/:id",
+    method: "GET",
+    id: req.params.id,
+  });
+});
+
+// Updates the post with the specified id using data from the request body. Returns the modified document, NOT the original.
+router.put("/:id", (req, res) => {
+  res.status(200).json({
+    router: "posts",
+    url: "/api/posts/:id",
+    method: "PUT",
+    id: req.params.id,
+  });
+});
+
+// Removes the post with the specified id and returns the deleted post object. You may need to make additional calls to the database in order to satisfy this requirement.
+router.delete("/:id", (req, res) => {
+  res.status(200).json({
+    router: "posts",
+    url: "/api/posts/:id",
+    method: "DELETE",
+    id: req.params.id,
+  });
+});
+
+// Returns an array of all the comment objects associated with the post with the specified id.
+router.get("/:id/comments", (req, res) => {
+  res.status(200).json({
+    router: "posts",
+    url: "/api/posts/:id/comments",
+    method: "GET",
+    id: req.params.id,
+  });
+});
+
+// Creates a comment for the post with the specified id using information sent inside of the request body.
+router.post("/:id/comments", (req, res) => {
+  res.status(200).json({
+    router: "posts",
+    url: "/api/posts/:id/comments",
+    method: "POST",
+    id: req.params.id,
+  });
+});
+
+module.exports = router;
